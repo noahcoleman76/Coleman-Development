44575288f52ef8a1a910d1c1ef64e84afecfd7b9
Noah Coleman <noahjcoleman25@gmail.com>
2026-06-29 15:39:27 -0600

    updated website redirect to knockoutpromos.com

--- scripts/redirect-colemandev.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var host = window.location.hostname.toLowerCase();
-  var isColemanDev = host === "colemandev.com" || host.endsWith(".colemandev.com");
-  var isPriority1 = host === "priority1.colemandev.com";
-
-  if (isColemanDev && !isPriority1) {
-    window.location.replace("https://knockoutpromos.com/");
-  }
-})();
